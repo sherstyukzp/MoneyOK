fba3cc289857e77db28075ebd295b3f969c1e389
Ярослав Шерстюк <sherstyuk.ya.n@gmail.com>
2022-02-20 22:58:57 +0200

    fix

--- MoneyOK/View/Accounts/AccountsListView.swift
+++ MoneyOK/View/Accounts/AccountsListView.swift
@@ -18,13 +18,42 @@ struct AccountsListView: View {
                     ])
     private var fetchedAccounts: FetchedResults<AccountEntity>
     
+    //
+    @FetchRequest(entity: TransactionEntity.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \TransactionEntity.transactionToCategory?.nameCategory, ascending: true)])
+    private var fetchedTransaction: FetchedResults<TransactionEntity>
+    
+    //
+    
+    @SectionedFetchRequest(
+        sectionIdentifier: \TransactionEntity.dateTransaction!,
+        sortDescriptors: [SortDescriptor(\TransactionEntity.dateTransaction, order: .reverse)])
+    
+    private var transactions: SectionedFetchResults<Date, TransactionEntity>
+    
+    
     @State private var query = ""
     
     var body: some View {
+        
         List {
-            if !(fetchedAccounts.filter{$0.isFavorite == true}).isEmpty {
-                Section("Избранные") {
-                    ForEach(fetchedAccounts.filter{$0.isFavorite == true && $0.isArchive == false}) { (account: AccountEntity) in
+            if query.isEmpty {
+                if !(fetchedAccounts.filter{$0.isFavorite == true}).isEmpty {
+                    Section("Избранные") {
+                        ForEach(fetchedAccounts.filter{$0.isFavorite == true && $0.isArchive == false}) { (account: AccountEntity) in
+                            NavigationLink(destination:
+                                            TransactionsView(accountItem: account).environment(\.managedObjectContext, self.viewContext))
+                            {
+                                AccountCallView(accountItem: account)
+                            }
+                            //.isDetailLink(false) // Исправляет баг с tabbar в списке транзакций
+                            .swipeActions() {}
+                        }
+                    }
+                }
+                
+                Section(fetchedAccounts.count <= 1 ? "Счёт" : "Счета") {
+                    ForEach(fetchedAccounts.filter{$0.isFavorite == false && $0.isArchive == false}) { (account: AccountEntity) in
                         NavigationLink(destination:
                                         TransactionsView(accountItem: account).environment(\.managedObjectContext, self.viewContext))
                         {
@@ -34,37 +63,36 @@ struct AccountsListView: View {
                         .swipeActions() {}
                     }
                 }
-            }
-            
-            Section(fetchedAccounts.count <= 1 ? "Счёт" : "Счета") {
-                ForEach(fetchedAccounts.filter{$0.isFavorite == false && $0.isArchive == false}) { (account: AccountEntity) in
-                    NavigationLink(destination:
-                                    TransactionsView(accountItem: account).environment(\.managedObjectContext, self.viewContext))
-                    {
-                        AccountCallView(accountItem: account)
+                if !(fetchedAccounts.filter{$0.isArchive == true}).isEmpty {
+                    Section("Архив") {
+                        ForEach(fetchedAccounts.filter{$0.isArchive == true}) { (account: AccountEntity) in
+                            NavigationLink(destination:
+                                            TransactionsView(accountItem: account).environment(\.managedObjectContext, self.viewContext))
+                            {
+                                AccountCallView(accountItem: account)
+                            }
+                            //.isDetailLink(false) // Исправляет баг с tabbar в списке транзакций
+                            .swipeActions() {}
+                        }
                     }
-                    //.isDetailLink(false) // Исправляет баг с tabbar в списке транзакций
+                }
+            }
+            else {
+                ForEach(fetchedTransaction, id: \.self) { item in
+                    NavigationLink(destination:
+                                    TransactionDetailView(transactionItem: item))
+                    {
+                        TransactionCallView(transactionItem: item)
+                    }
                     .swipeActions() {}
                 }
             }
-            if !(fetchedAccounts.filter{$0.isArchive == true}).isEmpty {
-                Section("Архив") {
-                    ForEach(fetchedAccounts.filter{$0.isArchive == true}) { (account: AccountEntity) in
-                        NavigationLink(destination:
-                                        TransactionsView(accountItem: account).environment(\.managedObjectContext, self.viewContext))
-                        {
-                            AccountCallView(accountItem: account)
-                        }
-                        //.isDetailLink(false) // Исправляет баг с tabbar в списке транзакций
-                        .swipeActions() {}
-                    }
-                }
-            }
+            
         }
-        .searchable(text: $query, prompt: "Поиск счёта")
-            .onChange(of: query) { newValue in
-                fetchedAccounts.nsPredicate = searchPredicate(query: newValue)
-            }
+        .searchable(text: $query, placement: .sidebar)
+        .onChange(of: query) { newValue in
+            fetchedTransaction.nsPredicate = searchPredicate(query: newValue)
+        }
         
     }
     
@@ -72,8 +100,8 @@ struct AccountsListView: View {
     private func searchPredicate(query: String) -> NSPredicate? {
         if query.isEmpty { return nil }
         return NSPredicate(format: "%K BEGINSWITH[cd] %@",
-                           #keyPath(AccountEntity.nameAccount), query)
-      }
+                           #keyPath(TransactionEntity.transactionToCategory.nameCategory), query)
+    }
     
     
     
