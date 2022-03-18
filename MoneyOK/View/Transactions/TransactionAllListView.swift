22ee78350d544e505af96a8b9c09937a234edd61
Ярослав Шерстюк <sherstyuk.ya.n@gmail.com>
2022-03-18 11:26:57 +0200

    fix sorted transactions

--- MoneyOK/View/Transactions/TransactionAllListView.swift
+++ MoneyOK/View/Transactions/TransactionAllListView.swift
@@ -8,6 +8,40 @@
 import SwiftUI
 
 
+extension TransactionEntity {
+    
+    @objc
+    public var sections: String {
+        // I used the base Xcode core data app which has timestamp as an optional.
+        // You can remove the unwrapping if your dates are not optional.
+        if let timestamp = dateTransaction {
+            // This sets up the RelativeDateTimeFormatter
+            let rdf = RelativeDateTimeFormatter()
+            // This gives the verbose response that you are looking for.
+            rdf.unitsStyle = .spellOut
+            // This gives the relative time in names like today".
+            rdf.dateTimeStyle = .named
+
+            // If you are happy with Apple's choices. uncomment the line below
+            // and remove everything else.
+  //        return rdf.localizedString(for: timestamp, relativeTo: Date())
+            
+            // You could also intercept Apple's labels for you own
+            switch rdf.localizedString(for: timestamp, relativeTo: Date()) {
+            case "now":
+                return "today"
+            case "in two days", "in three days", "in four days", "in five days", "in six days", "in seven days":
+                return "this week"
+            default:
+                return rdf.localizedString(for: timestamp, relativeTo: Date())
+            }
+        }
+        // This is only necessary with an optional date.
+        return "undated"
+    }
+}
+
+
 
 struct TransactionAllListView: View {
     
@@ -15,17 +49,21 @@ struct TransactionAllListView: View {
     @FetchRequest(entity: TransactionEntity.entity(),
                   sortDescriptors: [NSSortDescriptor(keyPath: \TransactionEntity.dateTransaction, ascending: true)])
     private var fetchedTransaction: FetchedResults<TransactionEntity>
+
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\TransactionEntity.dateTransaction, order: .reverse)
+    ]) var books: FetchedResults<TransactionEntity>
     
-//    @SectionedFetchRequest(
-//        sectionIdentifier: \TransactionEntity.transactionToCategory!.nameCategory!,
-//        sortDescriptors: [SortDescriptor(\TransactionEntity.transactionToCategory?.nameCategory, order: .reverse)])
     
     @SectionedFetchRequest(
-        sectionIdentifier: \TransactionEntity.dateTransaction!,
-        sortDescriptors: [SortDescriptor(\TransactionEntity.dateTransaction, order: .reverse)])
+        sectionIdentifier: \.sections,
+        sortDescriptors: [SortDescriptor(\TransactionEntity.dateTransaction, order: .reverse)],
+        animation: .default)
     
-    private var transactions: SectionedFetchResults<Date, TransactionEntity>
+    private var transactions: SectionedFetchResults<String, TransactionEntity>
 
+
+    //
     // Складывает все транзакции
     var sumTransaction: Double {
         fetchedTransaction.reduce(0) { $0 + $1.sumTransaction }
@@ -50,7 +88,7 @@ struct TransactionAllListView: View {
             } else {
                 List {
                     ForEach(transactions) { section in
-                        Section(header: Text(section.id, style: .date)) {
+                        Section(header: Text(section.id.capitalized)) {
                             ForEach(section) { item in
                                 NavigationLink(destination:
                                                 TransactionDetailView(transactionItem: item))
@@ -61,8 +99,9 @@ struct TransactionAllListView: View {
                                 }
                             }
                         }
+
                     }
-                }
+                }.listStyle(SidebarListStyle())
                 
 
                 
