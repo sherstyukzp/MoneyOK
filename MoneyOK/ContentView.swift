4cd802e50289b6d7ab36eb4883c6f2b05a88ef10
Ярослав Шерстюк <sherstyuk.ya.n@gmail.com>
2023-02-15 19:52:03 +0200

    - workng navigation

--- MoneyOK/ContentView.swift
+++ MoneyOK/ContentView.swift
@@ -36,23 +36,38 @@ struct ContentView: View {
                     Text(book.nameAccount ?? "Not sected")
                 }
             }
-            .navigationSplitViewColumnWidth(200)
+            //.navigationSplitViewColumnWidth(200)
+            .navigationSplitViewStyle(.balanced)
             .navigationTitle("Accounts")
             
         } content: {
-            List(selectedAccount?.transaction ?? [], selection: $selectedTransaction) { chapter in
-                NavigationLink(value: chapter) {
-                    Text(chapter.transactionToCategory?.nameCategory ?? "")
+            
+            if (selectedAccount != nil) {
+                List(selectedAccount?.transaction ?? [], selection: $selectedTransaction) { chapter in
+                    NavigationLink(value: chapter) {
+                        Text(chapter.transactionToCategory?.nameCategory ?? "")
+                    }
                 }
+                .navigationTitle("Transactions")
+            } else {
+                Text("Selected account")
             }
-            .navigationSplitViewColumnWidth(min: 400, ideal: 450, max:  500)
-            .navigationTitle("Chapters")
+            
+            //.navigationSplitViewColumnWidth(min: 400, ideal: 450, max:  500)
             
         } detail: {
-            Text("\(selectedTransaction?.sumTransaction ?? 0)" )
-                .padding()
-                .navigationTitle(selectedTransaction?.transactionToCategory?.nameCategory ?? "")
+            if selectedTransaction != nil {
+                Text("\(selectedTransaction?.sumTransaction ?? 0)" )
+                    .padding()
+                    .navigationTitle(selectedTransaction?.transactionToCategory?.nameCategory ?? "")
+            } else {
+                Text("Selected transaction")
+            }
+            
+            
         }
+        .navigationSplitViewStyle(.balanced)
+        
     }
 }
 
